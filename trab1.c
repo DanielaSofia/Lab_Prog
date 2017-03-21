cdddfc0c980844248fcc66903531a6c9e9a71316
Daniela Sofia <danielasofialobo@hotmail.com>
2017-03-21 15:09:33 +0000

    sub

--- trab1.c
+++ trab1.c
@@ -3,7 +3,18 @@
 #include<string.h>
 #include "polin.h"
 
-    Poli add(Poli *a,Poli *b){
+    Poli *add(Poli *a,Poli *b){
+        float soma;
+        
+        if((a->m->u.m.e == b->m->u.m.e) && (a->m->u.m.exp == (b->m->u.m.exp))){
+            soma = (a->m->u.m.coef) + (b->m->u.m.coef);
+             Mon *new = mk_node(soma,a->m->u.m.e,a->m->u.m.exp);
+        Poli *novo = mk_poli(new,b);
+            return novo;
+        }
+       
+        
+        
     }
 
     Poli derivate(Poli *p){
@@ -47,24 +58,34 @@
     }*/
 
 int main(){
-    Poli *p=null;
-    float coeficiente;
+    Poli *p=NULL;
+    float coeficiente, coeficiente1;
     char *var=malloc(sizeof(char) * 128);
-    int exp;
+    char *var1=malloc(sizeof(char) * 128);
+    int exp,exp1;
+    
     int n;
     printf("Introduza o numero de monomios a inserir:");
     scanf("%d",&n);
-    p->size=n;
+   // p->size=n;
     printf("Introduz os monomios a inserir na lista:");
     //for(int i=0;i<1;i++)
+     Mon *d;
+     Mon *t;
+    Poli *novo = mk_poli(d,p);
+     Poli *novo1 = mk_poli(t,novo);
      for(;p!=NULL;p=p->next)
         scanf("%f %c %d", &coeficiente ,&var, &exp);
+        scanf("%f %c %d", &coeficiente1 ,&var1, &exp1);
     
     //construtor
-        Mon *d = mk_node(coeficiente,var,exp);
-
+       d = mk_node(coeficiente,var,exp);
+   t = mk_node(coeficiente1,var1,exp1);
+    // Poli *novo = mk_poli(d,p);
+    // Poli *novo1 = mk_poli(t,novo);
+    Poli *sum = add(novo,novo1);
     //print
-   toString(d);
+   toString(sum->m);
         
     
     
